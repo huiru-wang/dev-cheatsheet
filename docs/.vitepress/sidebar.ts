ca8059a4d788eb9c29f2889a265236d23e6f7b95
huiru-wang <103855390+huiru-wang@users.noreply.github.com>
2023-09-13 16:15:05 +0800

    Update sidebar.ts

--- docs/.vitepress/sidebar.ts
+++ docs/.vitepress/sidebar.ts
@@ -6,7 +6,7 @@ export default [
             { text: '窗口函数', link: '/MySQL/窗口函数' },
             { text: '日期函数', link: '/MySQL/日期函数' },
             { text: '分组统计', link: '/MySQL/分组统计' },
-            { text: 'MyBatis常用模板', link: '/MySQL/MyBatis常用模板' }
+            { text: 'MySQL批量操作', link: '/MySQL批量操作' }
         ],
         collapsed: true,
     },
